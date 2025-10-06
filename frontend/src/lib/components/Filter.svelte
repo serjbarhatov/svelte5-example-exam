9f78143d87d8ec3d817a36b711d4314c0f05ecbb
serjbarhatov <barh0001@hz.nl>
2025-10-06 20:05:38 +0200

    Render pet types in fiter

--- frontend/src/lib/components/Filter.svelte
+++ frontend/src/lib/components/Filter.svelte
@@ -1,9 +1,13 @@
 <script>
 	import { getContext } from 'svelte';
+	let { petTypes } = $props();
 </script>
 
 <li>
-	<button class="m-2 bg-lime-400 p-1 hover:bg-amber-300"
-		>icon placeholder</button
-	>
+	<button class="m-2 bg-lime-400 p-1 hover:bg-amber-300">All</button>
+	{#each petTypes as petType}
+		<button class="m-2 bg-lime-400 p-1 hover:bg-amber-300">
+			{petType}
+		</button>
+	{/each}
 </li>
--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -8,14 +8,16 @@ export const load = async ({ fetch }) => {
     const petsResponse = await fetch(`${PUBLIC_API_URL}/pets`);
     const petsUrls = await petsResponse.json();
     let petsData = [];
+    const petTypes = new Set();
     for (let e of petsUrls) {
         const url = `${PUBLIC_API_URL}${e}`;
         console.log(url);
         const response = await fetch(url);
         const petData = await response.json();
+        petTypes.add(petData.type);
         petsData.push(petData);
     }
     console.log(petsUrls);
     console.log(petsData);
-    return { pets: petsData }
+    return { pets: petsData, petTypes: petTypes }
 };
