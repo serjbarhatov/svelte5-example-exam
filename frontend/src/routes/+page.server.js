2027619d0ee4a2a7de70b0e6d41e21890bcd4104
serjbarhatov <barh0001@hz.nl>
2025-10-14 17:02:39 +0200

    Call pets data asynchronously

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,21 +1,27 @@
 import { PUBLIC_API_URL } from "$env/static/public";
 
-/**
- * Function to load appointments
- * @returns {Promise<{ appointments: any[]; } | { error: any; }>}
- */
 export const load = async ({ fetch }) => {
     const petsResponse = await fetch(`${PUBLIC_API_URL}/pets`);
     const petsUrls = await petsResponse.json();
     let petsData = [];
+    let responsePromises = [];
     const petTypes = new Set(['All']);
     for (let e of petsUrls) {
         const url = `${PUBLIC_API_URL}${e}`;
         console.log(url);
-        const response = await fetch(url);
-        const petData = await response.json();
+        const responsePromise = fetch(url);
+        responsePromises.push(responsePromise);
+    }
+    let responses = await Promise.all(responsePromises);
+    let jsonPromises = [];
+    for (let response of responses) {
+        const jsonPromise = response.json();
+        jsonPromises.push(jsonPromise);
+    }
+    petsData = await Promise.all(jsonPromises)
+    for (let petData of petsData) {
         petTypes.add(petData.type);
-        petsData.push(petData);
+
     }
     console.log(petsUrls);
     console.log(petsData);
