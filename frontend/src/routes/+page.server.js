c285010391c36b1bb44bb898442aead24af25146
serjbarhatov <barh0001@hz.nl>
2025-10-14 19:19:42 +0200

    Refactor +page.server.js

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -3,27 +3,11 @@ import { PUBLIC_API_URL } from "$env/static/public";
 export const load = async ({ fetch }) => {
     const petsResponse = await fetch(`${PUBLIC_API_URL}/pets`);
     const petsUrls = await petsResponse.json();
-    let petsData = [];
-    let responsePromises = [];
-    const petTypes = new Set(['All']);
-    for (let e of petsUrls) {
-        const url = `${PUBLIC_API_URL}${e}`;
-        console.log(url);
-        const responsePromise = fetch(url);
-        responsePromises.push(responsePromise);
-    }
-    let responses = await Promise.all(responsePromises);
-    let jsonPromises = [];
-    for (let response of responses) {
-        const jsonPromise = response.json();
-        jsonPromises.push(jsonPromise);
-    }
-    petsData = await Promise.all(jsonPromises)
-    for (let petData of petsData) {
-        petTypes.add(petData.type);
-
-    }
-    console.log(petsUrls);
-    console.log(petsData);
+    const responsePromises = petsUrls.map(relativeUrl => fetch(`${PUBLIC_API_URL}${relativeUrl}`));
+    const responses = await Promise.all(responsePromises);
+    const jsonPromises = responses.map(response => response.json());
+    const petsData = await Promise.all(jsonPromises)
+    const allPetTypes = petsData.map(petData => petData.type);
+    const petTypes = new Set(['All', ...allPetTypes]);
     return { pets: petsData, petTypes: petTypes }
 };
