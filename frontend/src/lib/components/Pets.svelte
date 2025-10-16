80d38c05f49dcceabd8e862d755db0bb148207c9
serjbarhatov <barh0001@hz.nl>
2025-10-16 16:52:08 +0200

    Filter pets by type

--- frontend/src/lib/components/Pets.svelte
+++ frontend/src/lib/components/Pets.svelte
@@ -1,13 +1,19 @@
 <script>
-	import { getContext } from 'svelte';
+	import { setContext } from 'svelte';
 	import Filter from './Filter.svelte';
 	let { allPets } = $props();
+	let selectedPetType = $state('All');
+	const handleFilterSelect = (s) => {
+		selectedPetType = s;
+		console.log(selectedPetType);
+	};
+	setContext('pets', { handleFilterSelect });
 </script>
 
 <section class="flex flex-col">
 	<h3 class="text-center">Use this filter to switch between pets</h3>
 	<ul class="flex justify-center">
-		<Filter petTypes = {allPets.petTypes}/>
+		<Filter petTypes={allPets.petTypes} />
 	</ul>
 </section>
 <h2 class="my-6 text-center text-2xl">Dogs selected</h2>
@@ -21,11 +27,13 @@
 	</thead>
 	<tbody class="text-xs">
 		{#each allPets.pets as pet}
+		{#if selectedPetType == pet.type || selectedPetType == 'All'}
 			<tr>
 				<td>{pet.name}</td>
 				<td>{pet.type}</td>
 				<td>{pet.icon}</td>
 			</tr>
+			{/if}
 		{/each}
 	</tbody>
 </table>
