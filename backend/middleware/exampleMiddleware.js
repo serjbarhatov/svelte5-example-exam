3b08e009172c2526988e5a987f488dfcbeda05af
serjbarhatov <barh0001@hz.nl>
2025-10-17 14:09:53 +0200

    Log host name

--- backend/middleware/exampleMiddleware.js
+++ backend/middleware/exampleMiddleware.js
@@ -2,3 +2,8 @@ export async function checkName(req, res, next){
     console.log('I do not know you');
     next();
 }
+
+export function logHostname(req, res, next) {
+    console.log(`Client hostname: ${req.hostname}`);
+    next();
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllPets, updatePet, getPet, getSpiderIds, deletePets } from '../controllers/petsController.js';
-import { checkName } from '../middleware/exampleMiddleware.js';
+import { checkName, logHostname } from '../middleware/exampleMiddleware.js';
 import cors from 'cors';
 const router = express.Router();
 
@@ -8,10 +8,10 @@ const router = express.Router();
 router.get('/', cors(), (req, res, next) => {
   res.json('hi');
 });
-router.get('/pets', cors(), checkName, getAllPets);
-router.get('/pets/:id', cors(), checkName, getPet);
-router.put('/pets/:id', cors(), checkName, updatePet)
-router.get('/spiders', cors(), checkName, getSpiderIds);
-router.delete('/pets', cors(), checkName, deletePets);
+router.get('/pets', cors(), checkName, logHostname, getAllPets);
+router.get('/pets/:id', cors(), checkName, logHostname, getPet);
+router.put('/pets/:id', cors(), checkName, logHostname, updatePet)
+router.get('/spiders', cors(), checkName, logHostname, getSpiderIds);
+router.delete('/pets', cors(), checkName, logHostname, deletePets);
 
 export default router;
